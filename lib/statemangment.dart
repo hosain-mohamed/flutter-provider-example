4d386313d1d852b2efbb054dcedd9589259d321f
Hosain Mohamed <shs442000@gmail.com>
2020-06-17 20:08:54 +0200

    Update statemangment.dart

--- lib/statemangment.dart
+++ lib/statemangment.dart
@@ -2,7 +2,7 @@
 /*State management with Provider 
 
 1) ChangeNotifier 
-2) ChangeNofiterProvider
+2) ChangeNofitierProvider
 3) Consumer
 4) Provider.of
    
@@ -12,9 +12,8 @@
   
 //tips : 
   - put consumer deep in the widget tree to abvoid rebuild large part of UI
- 	- tips :  Just learn how to use ChangeNotifierProvider and Consumer. Every now and then you might use a Provider
-     widget if you don’t need to update the UI. Most of the time you won’t need a MultiProvider either if you have a
-     view model for each screen or page 
-	   And for injecting dependencies in your view models, GetIt will handle that. No need to worry about ProxyProvider
+  - tips :  Just learn how to use changeNotifier , ChangeNotifierProvider and Consumer. Every now and then you might use a Provider
+     	    widget if you don’t need to update the UI. Most of the time you won’t need a MultiProvider either if you have a view model for each screen or page 
+	    And for injecting dependencies in your view models, GetIt will handle that. No need to worry about ProxyProvider
 
 */
